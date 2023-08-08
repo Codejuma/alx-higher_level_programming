86fb93584e40a492a6f45afda29651ad012486b9
JUMA <jumaongoro@5gmail.com>
2023-08-08 13:07:02 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for a in range(1, 101):
-        if a % 3 == 0 and i % 5 == 0:
+        if a % 3 == 0 and a % 5 == 0:
             a = "FizzBuzz"
         elif a % 5 == 0:
             a = "Buzz"
