c1c976cc96679789dd2bb3d0bbd6c5ab3345dcc1
JUMA <jumaongoro@5gmail.com>
2023-08-16 13:10:17 -0700

    data structure

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    """Delete keys with a specific value in a dictionary."""
-    while value in a_dictionary.values():
-        for k, v in a_dictionary.items():
-            if v == value:
-                del a_dictionary[k]
-                break
-
-    return (a_dictionary)
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
